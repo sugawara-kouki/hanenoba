4e04ca57dcb3b06eda2108339f85f3c753f1be3e
sugawara-kouki <kou.720131@gmail.com>
2026-04-12 15:48:26 +0900

    I18n: Completed Japanese localization and fixed datetime translation missing errors. Set HTML lang='ja' globally.

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -6,10 +6,10 @@ class MypagesController < ApplicationController
     @base_date = params[:date]&.to_date || Date.today
     @start_date = @base_date.beginning_of_month.beginning_of_week(:sunday)
     @end_date = @base_date.end_of_month.end_of_week(:sunday)
-    
+
     # ユーザーが申し込んでいるイベントを取得
     @booked_events = current_user.booked_events.where(held_at: @start_date..@end_date.end_of_day)
-    
+
     # 日付ごとのマップを作成
     @events_by_date = @booked_events.group_by { |e| e.held_at.to_date }
   end
