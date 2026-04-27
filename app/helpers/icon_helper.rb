0cba139066ccc287649f43efb6c17c4d49a69732
sugawara-kouki <kou.720131@gmail.com>
2026-04-27 14:44:50 +0900

    Standardize public event views using IconHelper and localized dates

--- app/helpers/icon_helper.rb
+++ app/helpers/icon_helper.rb
@@ -37,6 +37,18 @@ module IconHelper
       content_tag(:svg, xmlns: "http://www.w3.org/2000/svg", fill: "none", viewBox: "0 0 24 24", stroke: "currentColor", class: classes) do
         tag(:path, stroke_linecap: "round", stroke_linejoin: "round", stroke_width: "2", d: "M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z")
       end
+    when :chevron_left
+      content_tag(:svg, xmlns: "http://www.w3.org/2000/svg", fill: "none", viewBox: "0 0 24 24", stroke: "currentColor", class: classes) do
+        tag(:path, stroke_linecap: "round", stroke_linejoin: "round", stroke_width: "3", d: "M15 19l-7-7 7-7")
+      end
+    when :arrow_right
+      content_tag(:svg, xmlns: "http://www.w3.org/2000/svg", fill: "none", viewBox: "0 0 24 24", stroke: "currentColor", class: classes) do
+        tag(:path, stroke_linecap: "round", stroke_linejoin: "round", stroke_width: "3", d: "M13 7l5 5m0 0l-5 5m5-5H6")
+      end
+    when :check
+      content_tag(:svg, xmlns: "http://www.w3.org/2000/svg", fill: "none", viewBox: "0 0 24 24", stroke: "currentColor", class: classes) do
+        tag(:path, stroke_linecap: "round", stroke_linejoin: "round", stroke_width: "4", d: "M5 13l4 4L19 7")
+      end
     when :tag
       content_tag(:svg, xmlns: "http://www.w3.org/2000/svg", fill: "none", viewBox: "0 0 24 24", stroke: "currentColor", class: classes) do
         tag(:path, stroke_linecap: "round", stroke_linejoin: "round", stroke_width: "2", d: "M7 7h.01M7 3h5c.512 0 1.024.195 1.414.586l7 7a2 2 0 010 2.828l-7 7a2 2 0 01-2.828 0l-7-7A1.994 1.994 0 013 12V7a4 4 0 014-4z")
