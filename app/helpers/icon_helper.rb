d856c973e0b02361d66aad4345e5ea2d198a4048
sugawara-kouki <kou.720131@gmail.com>
2026-04-28 12:53:31 +0900

    Add external_link icon to IconHelper

--- app/helpers/icon_helper.rb
+++ app/helpers/icon_helper.rb
@@ -111,6 +111,10 @@ module IconHelper
       content_tag(:svg, xmlns: "http://www.w3.org/2000/svg", fill: "none", viewBox: "0 0 24 24", stroke: "currentColor", class: classes) do
         tag(:path, stroke_linecap: "round", stroke_linejoin: "round", stroke_width: "2", d: "M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z")
       end
+    when :external_link
+      content_tag(:svg, xmlns: "http://www.w3.org/2000/svg", fill: "none", viewBox: "0 0 24 24", stroke: "currentColor", class: classes) do
+        tag(:path, stroke_linecap: "round", stroke_linejoin: "round", stroke_width: "2", d: "M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14")
+      end
     else
       "No Icon Found"
     end
